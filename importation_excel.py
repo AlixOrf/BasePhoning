3e5d324fb66946c5289c830fbb1ee5b3854c7f31
AlixOrf <alix.orfeuvre@epitech.digital>
2026-06-05 13:19:41 +0200

    UPT Tri

--- importation_excel.py
+++ importation_excel.py
@@ -131,7 +131,7 @@ if "complements" in df_raw.columns:
 for col in ["complements_est_association", "complements_est_service_public"]:
     if col not in df_raw.columns:
         df_raw[col] = "FAUX"
-        
+
 df_raw["etab_est_siege"] = (
     df_raw["etab_est_siege"]
     .astype(str)
@@ -142,6 +142,8 @@ df_raw["etab_est_siege"] = (
 mask_exclude = (
     (df_raw["complements_est_association"] == "VRAI") |
     (df_raw["complements_est_service_public"] == "VRAI") |
+    (df_raw["complements_est_ess"] == "VRAI") |
+    (df_raw["complements_est_administration"] == "VRAI") |
     (df_raw["etab_est_siege"] == "FALSE") |
     (df_raw["dirigeant_annee_de_naissance"] < "1954") |
     (df_raw["dirigeant_annee_de_naissance"] > "1972")
